1f010e34e23b272a2d73fc9ee2e2a0b27c5cfe73
kamakshijain <kamakshi@terpmail.umd.edu>
2019-10-13 19:17:38 -0400

    adding default library file

--- include/lib.hpp
+++ include/lib.hpp
@@ -1,8 +1,14 @@
+/**
+ * @author kamakshi jain
+ * @file data.hpp
+ * @copyright [2019] kamakshi jain
+ * @brief This is the stub for the data class
+ */
+
 #pragma once
 
 #include<iostream>
 
-void dummy()
-{
+void dummy() {
     std::cout << "HI" << std::endl;
 }
