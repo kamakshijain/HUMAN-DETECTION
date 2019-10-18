77cf7c04ebfcd827e7df421dc700683b7f6c0caa
Sayan Brahma <sbrahma0@gmail.com>
2019-10-17 21:42:05 -0400

    Task 2.2.2 Train class implementation - Adding getclassifier and gethogfeature functions

--- app/train.cpp
+++ app/train.cpp
@@ -26,3 +26,46 @@ Train::Train() {
     classifier->setC(0.01);  //  soft classifier
     classifier->setType(cv::ml::SVM::EPS_SVR);
 }
+
+/*
+ * @brief This function outputs the support vectors of the classifier.
+ * @return A double vector containing the support vectors.
+ */
+std::vector<double> Train::GetClassifier() {
+    //  get support vectors
+    cv::Mat svm = classifier->getSupportVectors();
+    //  get decision function
+    cv::Mat alpha, svidx;
+    double rho = classifier->getDecisionFunction(0, alpha, svidx);
+    //  create variable to return
+    std::vector<double> hogDetector(svm.cols+1);
+    memcpy(&hogDetector[0], svm.ptr(), svm.cols*sizeof(hogDetector[0]));
+    hogDetector[svm.cols] = static_cast<float>(-rho);
+    return hogDetector;
+}
+/*
+ * @brief This function extracts HOG descriptors from an image
+ * @param It is the window size used for HOG feature extraction
+ * @param Vector containing the images whose features need to be extracted
+ */
+void Train::GetHogFeatures(const cv::Size windowSize,
+        const std::vector<cv::Mat> & imgList) {
+    cv::HOGDescriptor hog;
+    hog.winSize = windowSize;
+    hog.cellSize = cv::Size(4, 4);
+    cv::Mat gray;
+    std::vector<float> descriptors;
+    for ( auto data : imgList )
+        if (data.cols >= windowSize.width && data.rows >=windowSize.height) {
+            // convert image to grayscale
+            cvtColor(data, gray, cv::COLOR_BGR2GRAY);
+            // extract hog features
+            hog.compute(gray, descriptors);
+            // store hog features
+            gradList.push_back(cv::Mat(descriptors).clone());
+            // flip the images
+            cv::flip(gray, gray, 1);
+            hog.compute(gray, descriptors);
+            gradList.push_back(cv::Mat(descriptors).clone());
+         }
+}
