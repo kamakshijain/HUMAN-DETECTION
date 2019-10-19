cee074b7964e5fd1933aca5f4130134b5aa98409
Sayan Brahma <sbrahma0@gmail.com>
2019-10-19 13:24:00 -0400

    Updating the main and cmakelist in the test folder

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,12 @@
+/**
+ * @author kamakshi jain
+ * @file main.cpp
+ * @copyright [2019] kamakshi jain
+ */
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
