3660eac5bab3bd777dc44f97f64dfd32c82aaca3
Sayan Brahma <47536918+sbrahma0@users.noreply.github.com>
2019-10-20 16:40:34 -0400

    Task 2.4.2 Updated Train class stub

--- include/train.hpp
+++ include/train.hpp
@@ -1,68 +1,63 @@
 /**
- * @author kamakshi jain
- * @file data.hpp
- * @copyright [2019] kamakshi jain
- * @brief This is the stub for the data class
+ * @author kamakshi jain, Sayan Brahma
+ * @file train.hpp
+ * @copyright [2019] kamakshi jain, Sayan Brahma
+ * @brief This is the stub for the Train class
  */
 
-#ifndef INCLUDE_DATA_HPP_
-#define INCLUDE_DATA_HPP_
+#ifndef INCLUDE_TRAIN_HPP_
+#define INCLUDE_TRAIN_HPP_
 
 #include <iostream>
 #include <vector>
 #include <opencv2/opencv.hpp>
 
-class Data {
-/**
- * @brief Public methods declaration
+/*
+ * @brief Train is a class
  */
- public:
+class Train {
 	/**
-	 * @brief Contructor for data class
+	 * @brief Public methods and constructors declaration
 	 */
-    Data();
+ public:
+    cv::Ptr<cv::ml::SVM> classifier;
+    std::vector<int> labels;
+	/**
+	 * @brief Constructor for the class Train
+	 */
+    Train();
     /**
-     * @brief Loads the positive training image set
-     * @param The path to the directory to the annotations
-     * @param The path to the positive images dataset
-     * @param Resizing window for the image
+     * @brief This function gives the support vectors of the classifier
+     * @return a float type vector
      */
-    void LoadPosImages(const cv::String, const cv::String,
-                           const cv::Size);
+    std::vector<float> getClassifier();
     /**
-     * @brief Loads the negative training set images
-     * @param The path to the directory to -ve images
-     * @param Resizing window for the image
+     * @brief This function is for extracting the HOG features from an image
+     * @param The window size for HOG feature extractor
+     * @param The image whose HOG features are to be extracted
      */
-    void LoadNegImages(const cv::String, const cv::Size);
+    void getHOGfeatures(const cv::Size, const std::vector<cv::Mat> &);
     /**
-     * @brief Destructor for data class
+     * @brief This function trains the SVM classifier from the HOG features
+     * @param If we want to save the classifier after training
+     * @param name of the classifier ,if to be saved
      */
-    ~Data();
+    void trainSVM(const bool, const cv::String);
+    /**
+     * @brief Destructor of the Train class
+     */
+    ~Train();
 
-  std::vector<cv::Mat> getNegImageList() const {
-    return negImageList;
-  }
+    const std::vector<cv::Mat>& getGradientList() const {
+    return gradientList;
+    }
 
-  void setNegImageList(std::vector<cv::Mat> negImageList) {
-    this->negImageList = negImageList;
-  }
+    void setGradientList(const std::vector<cv::Mat> &gradientList) {
+        this->gradientList = gradientList;
+    }
 
-  std::vector<cv::Mat> getPosImageList() const {
-    return posImageList;
-  }
-
-  void setPosImageList(std::vector<cv::Mat> posImageList) {
-    this->posImageList = posImageList;
-  }
-
-/**
- * @brief Private variables declaration
- */
  private:
-    std::vector<cv::Mat> posImageList, negImageList;
+    std::vector<cv::Mat> gradientList;
 };
 
-
-
-#endif  // INCLUDE_DATA_HPP_
+#endif  // INCLUDE_TRAIN_HPP_
