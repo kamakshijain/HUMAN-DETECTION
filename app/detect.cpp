912a79800f3263d7629e6525c2e169e176b3ab7f
Sayan Brahma <sbrahma0@gmail.com>
2019-10-21 14:17:57 -0400

    Task 3.1.1 Complete the license preambles for all files

--- app/detect.cpp
+++ app/detect.cpp
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2019 Sayan Brahma, Kamakshi Jain
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -132,7 +132,7 @@ cv::Rect Detect::testClassifier(const cv::String testDir, const cv::Size size,
 
         // Creating the Bounding Box
         std::cout << "Creating Bounding Boxes" << std::endl;
-        auto i = detections.begin(); 
+        auto i = detections.begin();
         while (i != detections.end()) {
             cv::Rect &r = *i;
             // std::cout << r << std::endl;  // display coordinates
@@ -170,3 +170,4 @@ cv::Rect Detect::testClassifier(const cv::String testDir, const cv::Size size,
 Detect::~Detect() {
     std::cout << "Class Detect has been Destroyed" << std::endl;
 }
+
