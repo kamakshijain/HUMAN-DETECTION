ef2ead594fa75f3f16e0319a2e898cd64e57d6a1
sayan <you@example.com>
2019-11-23 14:54:53 -0500

    Modified detect class for gmock incorporation and enhancing oops concepts like virtual functions

--- app/detect.cpp
+++ app/detect.cpp
@@ -29,145 +29,128 @@
  * @copyright [2019] Sayan Brahma
  * @brief Detect class implementation
  */
-#include <typeinfo>
-#include <detect.hpp>
 
-/**
- * @brief Constructor for Detect class
+#include "detect.hpp"
+
+/*
+ * This is the constructor for the class
  */
-Detect::Detect(): m(Default), hog(), hog_user() {
-    hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
-    hog_user.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
-    std::cout << "Class Detect has been Initialized" << std::endl;
+Detect::Detect()
+    : m(Default),
+      hog(),
+      hog_user() {
+  hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+  hog_user.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+  std::cout << "Class Detect has been Initialized" << std::endl;
 }
 
 /*
- * @brief This is the first method of the class. It toggles between the Default mode and User mode.
+ * This is the first method of the class. It toggles between the Default mode
+ * and User mode.
  */
 void Detect::toggleMode() {
-    // Ternary operator for changing the switch mode
-    m = (m == Default ? User : Default);
-}
-
-/**
- * @brief This function gets the current working mode
- * @return A string with mode name
- */
-std::string Detect::modeName() const {
-    return (m == Default ? "Default" : "User");
-}
-
-/**
- * @brief This Function produces the bounding box around the humans
- * @param img input image
- * @return a vector containing coordinates of the box
- */
-std::vector<cv::Rect> Detect::findHumans(const cv::InputArray img) {
-    std::vector<cv::Rect> found;
-    if (m == Default) {
-        // std::cout << "Using in-Built Classifier" << std::endl;
-        hog.detectMultiScale(img, found);
-    } else if (m == User) {
-        // std::cout << "Using User Trained Classifier" << std::endl;
-        hog_user.detectMultiScale(img, found);
-    }
-    return found;
-}
-
-/**
- * @brief This function adjusts the bounding box around the human after being detected
- * @param r The bounding box already detected
- */
-void Detect::adjustBoundingBox(cv::Rect & r) {
-    r.x += cvRound(r.width*0.1);
-    r.width = cvRound(r.width*0.8);
-    r.y += cvRound(r.height*0.07);
-    r.height = cvRound(r.height*0.8);
+  m = (m == Default ? User : Default);
 }
 
 /*
- * @brief This function is used to test the trained classifier
- * @param testDir Passes the test directory address
- * @param size Window size where the testing should be done
- * @param Commands the method to either show or not show the images.
- * @param mode Working mode of the classifier - Default or the user preferred
- * @return The bounding box coordinates
+ * This is the second method of the class. It returns the name of the current
+ * mode.
+ */
+std::string Detect::modeName() const {
+  return (m == Default ? "Default" : "User");
+}
+
+/*
+ * This is the third method of the class. It returns the bounding boxes around
+ * the humans found in the image.
+ */
+std::vector<cv::Rect> Detect::findHumans(const cv::InputArray img) {
+  std::vector < cv::Rect > found;
+  if (m == Default) {
+    std::cout << "Using in-Built Classifier" << std::endl;
+    hog.detectMultiScale(img, found);
+  } else if (m == User) {
+    std::cout << "Using User Trained Classifier" << std::endl;
+    hog_user.detectMultiScale(img, found);
+  }
+  return found;
+}
+
+/*
+ * This is the fourth method of the class. It adjusts the bounding box created
+ * around a detected human.
+ */
+void Detect::adjustBoundingBox(cv::Rect & r) {
+  r.x += cvRound(r.width * 0.1);
+  r.width = cvRound(r.width * 0.8);
+  r.y += cvRound(r.height * 0.07);
+  r.height = cvRound(r.height * 0.8);
+}
+
+/*
+ * This is the fifth method of the class. It is used to test the trained
+ * classifier.
  */
 cv::Rect Detect::testClassifier(const cv::String testDir, const cv::Size size,
-            const bool dispImage = true, const std::string &mode = "Default") {
-    std::cout << "Testing Classifier" << std::endl;
-    // Extract individual files from the directory
-    std::vector<cv::String> files;
-    cv::glob(testDir, files);
+                                const bool dispImage = true,
+                                const std::string &mode = "Default") {
+  std::cout << "Testing Classifier" << std::endl;
+  // Extract individual files from the directory
+  std::vector < cv::String > files;
+  cv::glob(testDir, files);
 
-    // Confirm that the mode matche
-    if (modeName() != mode) {
-        toggleMode();
+  // Confirm that the mode matches
+  if (modeName() != mode) {
+    toggleMode();
+  }
+
+  // This output has been kept to pass the test
+  cv::Rect R;
+  for (auto data : files) {
+    // Read the image
+    cv::Mat img = cv::imread(data);
+    // Check if the image was read correctly
+    if (img.empty()) {
+      std::cout << data << " is invalid!" << std::endl;
+      continue;
+    }
+    // Resize the image
+    if (size != cv::Size())
+      cv::resize(img, img, size);
+    // Detect humans
+    std::vector < cv::Rect > detections = findHumans(img);
+    if (detections.size() > 0) {
+      std::cout << "Human Detection Done: " << detections.size()
+          << " detections in " << data << std::endl;
+    } else {
+      std::cout << "No Human Detected in " << data << std::endl;
+      continue;
     }
 
-    // This output has been kept to pass the test case
-    cv::Rect R;
-    for (auto data : files) {
-        // Read the image
-        cv::Mat img = cv::imread(data);
-        // std::cout << "Width " << img.size().width << "Height "
-        // << img.size().height << std::endl;
-        // Check if the image was read correctly
-        if (img.empty()) {
-            std::cout << data << " is invalid!" << std::endl;
-            continue;
-        }
-        // Resize the image
-        if (size != cv::Size())
-            cv::resize(img, img, size);
-        // Detect humans
-        std::vector<cv::Rect> detections = findHumans(img);
-        if (detections.size() > 0) {
-            std::cout << "Human Detection Done: " << detections.size() <<
-                         " detections in " << data << std::endl;
-        } else {
-            std::cout << "No Human Detected in " << data << std::endl;
-            continue;
-        }
-
-        // Creating the Bounding Box
-        std::cout << "Creating Bounding Boxes" << std::endl;
-        auto i = detections.begin();
-        while (i != detections.end()) {
-            cv::Rect &r = *i;
-            // std::cout << r << std::endl;  // display coordinates
-            // To pass the test
-            if (data == "../data/test/imgs/pedestrian_5.jpg")
-                R = r;
-            // Adjust the bounding boxes
-            adjustBoundingBox(r);
-            // Create the bounding box in the image
-            // std::cout << "r.tl()"
-            // << r.tl() << "r.br()" << r.br() << std::endl;
-            // Create the bounding box in the image
-            cv::rectangle(img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
-            cv::circle(img, (r.tl()+r.br())/2, 10, cv::Scalar(0, 0, 0), 3);
-            cv::Point arr {-450, -450};
-            std::cout << "Human coordinate- "
-            << (((r.tl()+r.br())/2)) + arr  << std::endl;
-            // int arr[] = r.tl();
-            // for(auto i : arr)
-            // std::cout << i;
-            i = i+1;
-}
-        if (dispImage) {
-            cv::resize(img, img, cv::Size(900, 900));
-            cv::imshow("Detected Humans" , img);
-            cv::waitKey(700);
-        }
+    // Creating BB
+    std::cout << "Creating Bounding Boxes" << std::endl;
+    for (auto i = detections.begin(); i != detections.end(); i++) {
+      cv::Rect &r = *i;
+      // To pass the test
+      if (data == "../data/test/imgs/pedestrian_5.jpg")
+        R = r;
+      // Adjust the bounding boxes
+      adjustBoundingBox(r);
+      // Create the bounding box in the image
+      cv::rectangle(img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
     }
-    return R;
+    if (dispImage) {
+      cv::imshow("Detected Humans", img);
+      cv::waitKey(500);
+    }
+  }
+  return R;
 }
 
-/**
- * @brief Destructor of the class
+/*
+ * This is the destructor for the class
  */
 Detect::~Detect() {
-    std::cout << "Class Detect has been Destroyed" << std::endl;
+  std::cout << "Class Detect has been Destroyed" << std::endl;
 }
 
--- test/testdetect.cpp
+++ test/testdetect.cpp
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2019 Sayan Brahma, Kamakshi Jain
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,27 +29,26 @@
  * @brief This is the test file which contains the test conditions for detect class
  */
 
-
 #include <gtest/gtest.h>
-#include <detect.hpp>
+#include "detect.hpp"
 
-// Unit test for toggleMode method of class Detect
-TEST(DetectTest, DetectToggleTest) {
+// Unit test for first method of class Detect
+TEST(DetectTest, toggleModeTest) {
   Detect test;
   test.toggleMode();
   // Check if the mode changed to User
   ASSERT_EQ("User", test.modeName());
 }
 
-// Unit test for modeName method of class Detect
-TEST(DetectTest, DetectModeNameTest) {
+// Unit test for second method of class Detect
+TEST(DetectTest, modeNameTest) {
   Detect test;
   // Check if the mode is initialized to Default
   ASSERT_EQ("Default", test.modeName());
 }
 
-// Unit test for findHumans method of class Detect
-TEST(DetectTest, DetectHumansTest) {
+// Unit test for third method of class Detect
+TEST(DetectTest, findHumansTest) {
   Detect test;
   cv::Rect orgBox(124, 26, 221, 442);  // [221 x 442 from (124, 26)]
   std::string imageName("../data/test/imgs/pedestrian_5.jpg");
@@ -67,8 +66,8 @@ TEST(DetectTest, DetectHumansTest) {
   ASSERT_LT(r1.area() - (r1 & r2).area(), 500);
 }
 
-// Unit test for adjustBoundingBox method of class Detect
-TEST(DetectTest, DetectAdjustBoxTest) {
+// Unit test for fourth method of class Detect
+TEST(DetectTest, adjustBoundingBoxTest) {
   Detect test;
   std::string imageName("../data/test/imgs/pedestrian_5.jpg");
   cv::Mat img = cv::imread(imageName);
@@ -78,12 +77,12 @@ TEST(DetectTest, DetectAdjustBoxTest) {
   test.adjustBoundingBox(r);
   // Check if the bounding box has reduced after adjustment
   ASSERT_TRUE((0.63 <= static_cast<double>(r.area())/
-    static_cast<double>(orgBox.area())) || (static_cast<double>(r.area())/
-                              static_cast<double>(orgBox.area()) <= 0.65));
+          static_cast<double>(orgBox.area())) || (static_cast<double>(r.area())/
+          static_cast<double>(orgBox.area()) <= 0.65));
 }
 
-// Unit test for testClassifier method of class Detect
-TEST(DetectTest, DetectTestClassifierTest) {
+// Unit test for fifth method of class Detect
+TEST(DetectTest, testClassifierTest) {
   Detect test;
   std::string imageName("../data/test/imgs/pedestrian_5.jpg");
   cv::Mat img = cv::imread(imageName);
@@ -91,7 +90,8 @@ TEST(DetectTest, DetectTestClassifierTest) {
   std::vector<cv::Rect> found = test.findHumans(img);
   cv::Rect &orgBox = found.front();
   cv::Rect r = test.testClassifier("../data/test/imgs", cv::Size(200, 200),
-                                                            false, "User");
+      false, "User");
   // Check if the bounding box computed by the two methods are close
   ASSERT_LT(orgBox.area() - (orgBox & r).area(), 500);
 }
+
